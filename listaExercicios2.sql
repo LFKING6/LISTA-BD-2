26b06def5e73bf99cd18d9c7dbdbd4b3dbd420e6
Luis Felipe <luisfelipe.tl2006@gmail.com>
2023-09-26 23:27:21 -0300

    exercicio 2 feito

--- listaExercicios2.sql
+++ listaExercicios2.sql
@@ -4,6 +4,15 @@ BEGIN
     SELECT * FROM Autor;
 END //
 DELIMITER ;
-
-
 CALL sp_ListarAutores();
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+CALL sp_LivrosPorCategoria('Romance');
